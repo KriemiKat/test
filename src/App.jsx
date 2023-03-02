48fc893d4d12c736bd58705dbaf657488d249367
Katažina <katazina.fukalova@gmail.com>
2023-03-02 22:39:33 +0200

    test

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Labukas from './Components/Labas';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Labukas/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Labas.jsx
@@ -0,0 +1,4 @@
+function Labukas() {
+  return <h1>Sveiki, gyvi :) </h1>;
+}
+export default Labukas;
