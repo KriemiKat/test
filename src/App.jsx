0272c3d5a6d80cb6b46b8da0b884aa79176f1c2a
Katažina <katazina.fukalova@gmail.com>
2023-03-02 23:53:55 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
-import Labukas from './Components/Labas';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Labukas/>
+      <ul className="list-group">
+  <li className="list-group-item">Morkos</li>
+  <li className="list-group-item">Bananai</li>
+  <li className="list-group-item">Pienas</li>
+  <li className="list-group-item">Salotos</li>
+  <li className="list-group-item">Obuoliai</li>
+</ul>
       </header>
     </div>
   );
